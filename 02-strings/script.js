fa8ae07ece8e55642046aa0cb1a287edf73980dd
aldodenegri <aldodenegri00@gmail.com>
2025-10-24 12:37:11 +0200

    correzuine stringhe:1 e aggiunta stringhe:2

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,12 +1,14 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
-const name='Aldo';
+let name='Aldo';
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
-
+let number=25;
+let text='Ciao';
+let bool=true;
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
